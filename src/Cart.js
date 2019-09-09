a5f98e3591f5df9aad298080f6968166faca2f28
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-09-09 09:38:08 -0700

    initial architecture

---
+++ src/Cart.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Cart(props) {
+    const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      });
+    const summary = Object.keys(this.props.currentState.selected).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const selectedOption = this.props.currentState.selected[feature];
+  
+        return (
+          <div className="summary__option" key={featureHash}>
+            <div className="summary__option__label">{feature} </div>
+            <div className="summary__option__value">{selectedOption.name}</div>
+            <div className="summary__option__cost">
+              {USCurrencyFormat.format(selectedOption.cost)}
+            </div>
+          </div>
+        );
+      });
+  
+      const total = Object.keys(this.props.currentState.selected).reduce(
+        (acc, curr) => acc + this.props.currentState.selected[curr].cost,
+        0
+      );
+  
+      return (
+        <div className="App">
+          <header>
+            <h1>ELF Computing | Laptops</h1>
+          </header>
+          <main>
+            <form className="main__form">
+              <h2>Customize your laptop</h2>
+              {this.props.features}
+            </form>
+            <section className="main__summary">
+              <h2>Your cart</h2>
+              {summary}
+              <div className="summary__total">
+                <div className="summary__total__label">Total</div>
+                <div className="summary__total__value">
+                  {USCurrencyFormat.format(total)}
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      );
+}
